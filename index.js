d1e6ff014971ed723ef916e7a64fd6aa96a4db70
Your Name <you@example.com>
2021-09-13 10:31:18 -0600

    modifying

--- index.js
+++ index.js
@@ -9,11 +9,25 @@ let formElements= document.querySelectorAll(".formElement");
 console.log( formElements);
 
 
-let country= document.querySelector("@userCountry");
+let country= document.querySelector('#userCountry');
 console.log(country);
 
 
 console.log(country.value);
 console.log(listElements[0].textContent);
+
+listElements[0].textContent= "Changed through JS";
+console.log(listElements[0].textContent);
+
+let menu = document.querySelectorAll('ul.menu');
+console.log ( menu.innerHTML) += "<li> Logout </li>";
+
+/*
+${new tex here} to avoid using the + symbol in Strings
+*/
+
+
+
+
 
 
