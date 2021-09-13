352626cc5e4b8c46497c8b18ac78478b9780ea30
Your Name <you@example.com>
2021-09-13 10:16:23 -0600

    adding file

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("This is linked correctly!");
+
+ let listElements= document.querySelectorAll("li");
+
+ console.log(listElements);
+
+
+let formElements= document.querySelectorAll(".formElement");
+console.log( formElements);
+
+
+let country= document.querySelector("@userCountry");
+console.log(country);
+
+
+console.log(country.value);
+console.log(listElements[0].textContent);
+
+
